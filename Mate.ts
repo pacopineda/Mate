3acff6b82bb12b787fef255fb7022180050effe7
pacopineda <48750934+pacopineda@users.noreply.github.com>
2024-02-12 20:22:55 +0100

    Update Mate.ts

--- Mate.ts
+++ Mate.ts
@@ -81,37 +81,37 @@ namespace Mate {
 */    
      if (direction == 0 && index == 0){
             speed1 = lento;
-            spin = speed + 87;
+            spin = speed + 86;
             pins.servoWritePin(AnalogPin.P0, spin);
         }
       if (direction == 1 && index == 0) {
               speed1 = rapido;
-              spin = 87 - speed;
+              spin = 86 - speed;
             pins.servoWritePin(AnalogPin.P0, spin);
         }
       if (direction == 0 && index == 1) {
              speed2 = rapido;
-             spin2 = 87 - speed;
+             spin2 = 86 - speed;
             pins.servoWritePin(AnalogPin.P8, spin2);
         }
        if (direction == 1 && index == 1) {
              speed2 = lento;
-             spin2 = speed + 87;
+             spin2 = speed + 86;
             pins.servoWritePin(AnalogPin.P8, spin2);
         }
         if (direction == 0 && index == 2) {
              speed1 = lento;
              speed2 = rapido;
-             spin = speed +87;
-             spin2 = 87 - speed;
+             spin = speed +86;
+             spin2 = 86 - speed;
             pins.servoWritePin(AnalogPin.P0, spin);
             pins.servoWritePin(AnalogPin.P8, spin2);
         }
         if (direction == 1 && index == 2) {
              speed1 = rapido;
              speed2 = lento;
-             spin = 87 - speed;
-             spin2 = speed + 87;
+             spin = 86 - speed;
+             spin2 = speed + 86;
             pins.servoWritePin(AnalogPin.P0, spin);
             pins.servoWritePin(AnalogPin.P8, spin2);
         }
